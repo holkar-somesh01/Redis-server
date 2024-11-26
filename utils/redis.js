99abf21e8e2cc9501823a6f4ff09dae18406fc71
someshwarsholkar <someshwarsholkar22@gmail.com>
2024-11-26 11:22:24 +0530

    host name update

--- utils/redis.js
+++ utils/redis.js
@@ -2,7 +2,7 @@ const { default: Redis } = require("ioredis")
 require("dotenv").config()
 
 const redis = new Redis({
-    host: process.env.REDIS_HOST || 'localhost',
+    host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT || 6379,
 })
 redis.on('error', (err) => {
